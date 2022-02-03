558694c64b035d746b191d226ffc59fdb4e9df58
Bobby Grayson <bobbbygrayson@gmail.com>
2022-02-02 23:40:16 -0500

    Add some things to get things fully converted to html for all posts, test it

--- lib/work_log.ex
+++ lib/work_log.ex
@@ -5,10 +5,88 @@ defmodule WorkLog do
   I probably will just wrap this all up as an escript and/or throw a makefile around it
   """
 
-  @doc """
-  Returns the years which there are entries for
-  """
+  @valid_months [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
+  @valid_years [
+    2022,
+    2023,
+    2024,
+    2025,
+    2026
+  ]
+
+  def valid_months, do: @valid_months
+  def valid_years, do: @valid_years
+
+  def generate_years_posts(year) do
+    @valid_years
+    |> Enum.map(fn year ->
+      @valid_months
+      |> Enum.map(&__MODULE__.html_for_month_and_year(year, &1))
+    end)
+  end
+
+  @doc """
+  Generates HTML for a given year/months posts
+
+  From here, you can do whatever you please with the content
+
+  The default high level API will construct one large index.html file
+
+  Once I make it smarter, they will each become a route and be a part of a Plug
+  that can be served however you please.
+
+  The V1 here just spits out an index.html and isnt there yet
+  """
+  @spec html_for_month_and_year(pos_integer(), pos_integer()) ::
+          {:ok, [String.t()]} | {:error, any()}
+  def html_for_month_and_year(year, month) do
+    case post_files_by_month(year, month) do
+      {:error, e} ->
+        {:error, e}
+
+      result ->
+        result
+        |> Enum.map(&File.read!/1)
+        |> Enum.map(&Earmark.as_html!/1)
+    end
+  end
+
+  @doc """
+  Get the priv-based path to all entries for a given month or year
+
+  Met to be composed with markdown conversion, which is the next step
+  """
+  @spec post_files_by_month(pos_integer(), pos_integer()) :: {:ok, [String.t()]} | {:error, any}
+  def post_files_by_month(year, month)
+      when is_integer(year) and
+             month in @valid_months and
+             year in @valid_years do
+    case Enum.any?(years(:integer), fn y -> year == y end) do
+      true ->
+        month_string = month_integer_folder_name(month)
+
+        case File.ls("priv/years/#{year}/#{month_string}") do
+          {:ok, posts} ->
+            posts
+            |> Enum.map(fn post_file -> "priv/years/#{year}/#{month_string}/#{post_file}" end)
+
+          error ->
+            error
+        end
+
+      # somehow this branch isnt being hit, but who cares check later
+      false ->
+        {:error, "Invalid year provided"}
+    end
+  end
+
+  @doc false
+  def post_files_by_month(_, _) do
+    {:error, "error, either month or year folder does not exist in priv"}
+  end
+
+  @doc false
   @spec years(atom()) :: [pos_integer()] | {:error, any}
   def years(:integer) do
     case File.ls("priv/years") do
@@ -18,6 +96,7 @@ defmodule WorkLog do
   end
 
   @spec years() :: [String.t()] | {:error, any}
+  @doc false
   def years() do
     case File.ls("priv/years") do
       {:ok, files} -> files |> Enum.sort()
@@ -25,4 +104,17 @@ defmodule WorkLog do
     end
   end
 
+  @spec month_integer_folder_name(pos_integer()) :: String.t()
+  def month_integer_folder_name(1), do: "01"
+  def month_integer_folder_name(2), do: "02"
+  def month_integer_folder_name(3), do: "03"
+  def month_integer_folder_name(4), do: "04"
+  def month_integer_folder_name(5), do: "05"
+  def month_integer_folder_name(6), do: "06"
+  def month_integer_folder_name(7), do: "07"
+  def month_integer_folder_name(8), do: "08"
+  def month_integer_folder_name(9), do: "09"
+  def month_integer_folder_name(10), do: "10"
+  def month_integer_folder_name(11), do: "11"
+  def month_integer_folder_name(12), do: "12"
 end
--- test/work_log_test.exs
+++ test/work_log_test.exs
@@ -1,5 +1,11 @@
 defmodule WorkLogTest do
+
   use ExUnit.Case
+
+  @moduletag timeout: :infinity
+
+  @year 2022
+  @month 2
 
   test "can get the given years with entries, giving integers if asked" do
     assert WorkLog.years(:integer) == [2022, 2023, 2024, 2025, 2026]
@@ -7,5 +13,46 @@ defmodule WorkLogTest do
 
   test "can get the given years with entries, defaulting to strings" do
     assert WorkLog.years() == ~w(2022 2023 2024 2025 2026)
+  end
+
+  test "it can get the posts for a given year/month combo if they exist" do
+    [first_post | _] = WorkLog.post_files_by_month(@year, @month)
+    assert first_post in ["priv/years/2022/02/02.md"]
+  end
+
+  test "it returns a proper error if the there are no posts in that month or year" do
+    {:error, e} = WorkLog.post_files_by_month(2099, @month)
+    assert e == "error, either month or year folder does not exist in priv"
+  end
+
+  test "it can get HTML files for a month and year" do
+    res = WorkLog.html_for_month_and_year(@year, @month)
+    expected = ["<h1>\nA starting point</h1>\n<h2>\nLets see how this works</h2>\n<p>\nIt is where I would normally keep some notes</p>\n"]
+    assert res == expected
+  end
+
+  test "generates HTML for a whole year" do
+    [
+      [
+        _january,
+        [feb_2_html | _rest],
+        _march,
+        _april,
+        _may,
+        _june,
+        _july,
+        _august,
+        _september,
+        _october,
+        _novemmber,
+        _december,
+      ],
+      _twenty_twenty_three,
+      _twenty_twenty_four,
+      _twenty_twenty_five,
+      _twenty_twenty_six,
+    ] = WorkLog.generate_years_posts(2022)
+    expected_html = "<h1>\nA starting point</h1>\n<h2>\nLets see how this works</h2>\n<p>\nIt is where I would normally keep some notes</p>\n"
+    assert feb_2_html == expected_html
   end
 end
